b3556875f6efa814414d77394316241961aec24e
Evgeny Ivanov <busforward@gmail.com>
2017-08-09 14:21:25 +0500

    слайдер презентации

--- js/function.js
+++ js/function.js
@@ -105,6 +105,55 @@ $(document).ready(function() {
 		dots: true
 	});
 
+	$('.presentation__slider').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		adaptiveHeight: true,
+		// arrows: false,
+		nextArrow: $('.modal-footer_next'),
+		prevArrow: $('.modal-footer_prev')
+	});
+
+	$('#modal__presentation').on('shown.bs.modal', function (e) {	
+		$('.presentation__slider').resize();
+		presntationSliderBtn('.presentation__slider');
+	});
+
+	function presntationSliderBtn(el) {
+		var slider = $(el);
+		var sliderItem = slider.find('.slick-slide');
+		var firstSlide = sliderItem.first();
+		var lastSlide = sliderItem.last();
+		var itemCount = $.each(sliderItem, function(index, val) {
+			 count =+ index;
+		});
+		var currentSlide = slider.slick('slickCurrentSlide');
+
+		if (currentSlide==0) {
+			$('.presentation__sliderfoot_first').show();
+			$('.presentation__sliderfoot_next').hide();
+		} else {
+			$('.presentation__sliderfoot_first').hide();
+			$('.presentation__sliderfoot_next').show();
+		}
+
+		slider.on('afterChange', function(){
+			var current = slider.slick('slickCurrentSlide');
+			if (current==0) {
+				$('.presentation__sliderfoot_first').show();
+				$('.presentation__sliderfoot_next').hide();
+			} else {
+				$('.presentation__sliderfoot_next_count').find('.current').text(current);
+				$('.presentation__sliderfoot_next_count').find('.total').text(itemCount.length-1);
+
+				$('.presentation__sliderfoot_first').hide();
+				$('.presentation__sliderfoot_next').show();
+			}
+		});
+	}
+
 	$('.slider__grid').slick({
 		infinite: false,
 		// lazyLoad: 'progressive',
@@ -853,11 +902,5 @@ $(document).ready(function() {
 	// 	});
 	// 	return false;
 	// });
-
-	$('.presentation__slider').slick({
-		infinite: false,
-		slidesToShow: 1,
-		slidesToScroll: 1
-    })
 
 });
